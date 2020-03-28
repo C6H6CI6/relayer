934066d6fe0535a141df1571e189273f0ce54e7b
LycrusHamster <liang2929@hotmail.com>
2020-03-29 06:54:33 +0800

    build 3

--- src/job_sync_block_to_muta.js
+++ src/job_sync_block_to_muta.js
@@ -26,7 +26,7 @@ async function job_sync_block_to_muta () {
     let blocks = await dao.get_ckb_blocks(last_committed + 1, last_synced);
     headers = blocks.map((block) => block.header);
     //console.log(`job_sync_block_to_muta, commit [${last_committed + 1},${last_synced}] to Muta`);
-    mutaCaller.commit_ckb_header(headers);
+    await mutaCaller.commit_ckb_header(headers);
   }catch (e) {
     this.sync_block_to_muta_lock = false;
   }
